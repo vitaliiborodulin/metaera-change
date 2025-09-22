6acee6e7f64637d7995663846d47262e79f2976c
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-09-22 10:02:56 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,78 @@ $(function() {
 
     
     $('.banner__content').on('click', function(){
-        $('.banner').slideUp();
+        // $('.banner').slideUp();
+        $('.banner').hide();
     });
+
+    // const container = document.querySelector(".price__list");
+    // const options = {
+    //     enabled: true,
+    //     breakpoints: {
+    //       "(min-width: 576px)": {
+    //         enabled: false,
+    //       },
+    //     },
+    // };
+    
+    // Carousel(container, options).init();
+    
+    // const priceSlider = new Swiper('.swiper', {
+    //     // loop: true,
+    //     // enabled: false,
+    //     slidesPerView: 1.5,
+    //     spaceBetween: 16,
+    //     // breakpoints: {
+    //     //     1200: {
+    //     //         slidesPerView: 3.2,
+    //     //         spaceBetween: 24,
+    //     //     },
+    //     //     894: {
+    //     //         slidesPerView: 1.6,
+    //     //         spaceBetween: 20,
+    //     //     },
+    //     // },
+    //     // navigation: {
+    //     //   nextEl: '.swiper-button-next',
+    //     //   prevEl: '.swiper-button-prev',
+    //     // },
+    // });
+    
+    // priceSlider.disable();
+    
+    // $(window).resize(function(){
+    //     priceSlider.update();
+    // });
+    
+    let swiperinit = false;
+    let mobileswiper;
+    
+    function swiperOn() {
+        if (window.innerWidth <= 576) {
+            $('.price__list').wrapInner('<div class="swiper-wrapper"></div>');
+            swiperinit = true;
+            mobileswiper = new Swiper('.price__list', {
+                slideClass: 'price__item',
+                slidesPerView: 1.4,
+                spaceBetween: 16,
+                // breakpoints: {
+                //     0: {
+                //         slidesPerView: 1,
+                //     },
+                //     576: {
+                //         slidesPerView: 2,
+                //     }
+                // },   
+            });
+        } else if (swiperinit) {
+            $('.price__list .swiper-wrapper').contents().unwrap();
+            $('.price__list .price__item').removeAttr('style');    
+            //mobileswiper.destroy(true, true); 
+            swiperinit = false;
+        }
+    }
+    
+    swiperOn();
+    $(window).resize(swiperOn);
 
 });
