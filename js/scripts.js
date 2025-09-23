c62cc70f72c50ffbc348fb4c99a2435031fd5b3d
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-09-23 10:38:35 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,14 @@ $(function() {
         $('.banner').hide();
     });
 
+    $('.tabs-wrapper').each(function() {
+      let ths = $(this);
+      ths.find('.tab-item').not(':first').hide();
+      ths.find('.tab').click(function() {
+        ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
+        ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
+      }).eq(0).addClass('active');
+    });
     // const container = document.querySelector(".price__list");
     // const options = {
     //     enabled: true,
