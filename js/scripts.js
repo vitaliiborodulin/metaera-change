d66211e0d1c8cd97db5d4ece74b28faf30fed061
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2025-10-06 12:27:24 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -146,5 +146,71 @@ $(function() {
     
     // swiperOn1();
     // $(window).resize(swiperOn1);
+    // let min = 400;
+    
+    
+    // if ($(window).width() <= '1280'){
+    //     min = 360;
+    // } else if ($(window).width() <= '576'){
+    //     min = 300;
+    // }
+    
+    // $(".ta").each(function(indx, el) {
+    
+    //     let b = $(el),
+    //         max = el.scrollHeight,
+    //         a = b.next(".read-next");
+    //     if (max <= min) a.hide();
+    //     else a.on("click", function(event) {
+    //         let h = b.height();
+    //         b.height(h < max ? max : min);
+    //         $(this).text(h < max ? "Свернуть" : "Развернуть");
+    //         return false
+    //     })
+    // });
+    
+    // $('.tab').on('click', function(){
+    
+    //     $(".ta").each(function(indx, el) {
+    
+    //         let b = $(el),
+    //             max = el.scrollHeight,
+    //             a = b.next(".read-next");
+    //             a.show();
+    //         if (max <= min) a.hide();
+    //         else a.on("click", function(event) {
+    //             let h = b.height();
+    //             b.height(h < max ? max : min);
+    //             $(this).text(h < max ? "Свернуть" : "Развернуть");
+    //             return false
+    //         })
+    //     });
+    
+    // });
+    
+    let min = 180;
+    
+    if ($(window).width() <= '576'){
+    
+        $(".agree__item-descr").each(function(indx, el) {
+    
+            let b = $(el),
+                max = el.scrollHeight,
+                a = b.next(".read-next");
+            if (max <= min) a.hide();
+            else a.on("click", function(event) {
+                let h = b.height();
+                b.height(h < max ? max : min);
+                $(this).text(h < max ? "Свернуть" : "Развернуть");
+                return false
+            })
+        });
+    
+        
+    } else {
+    
+        $(".agree__item .read-next").hide();
+    
+    }
 
 });
